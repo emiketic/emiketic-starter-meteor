a5868ed1646a0016623283bf286f116fa7d6bab1
Mehdi Mahmoudi <mehdi@emiketic.com>
2021-09-14 16:29:15 +0100

    feat: Added style with scsss

--- imports/ui/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const App = () => (
-  <div>
-    <h1>EMIKETIC Starter Meteor</h1>
-  </div>
-);
---
+++ imports/ui/App/index.js
@@ -0,0 +1,22 @@
+/* -------------------------------------------------------------------------- */
+/*                                Dependencies                                */
+/* -------------------------------------------------------------------------- */
+
+// Packages
+import React from 'react';
+
+// Style
+import './index.scss';
+
+/* -------------------------------------------------------------------------- */
+/*                                  Component                                 */
+/* -------------------------------------------------------------------------- */
+/**
+ * Main application component
+ * @returns {React.ELement} Application root component
+ */
+export const App = () => (
+  <div className="app">
+    <h1>EMIKETIC Starter Meteor</h1>
+  </div>
+);
